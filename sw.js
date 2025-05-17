2758d202e0a5e3ed50a250a2068d6b9299a5d74d
Galileo <tavishberlin@outlook.com>
2025-05-17 16:52:29 -0500

    maybe

--- sw.js
+++ sw.js
@@ -1,9 +1,22 @@
 
-const cacheName = "gridiron-cache";
-const assets = ["index.html", "style.css", "game.js", "manifest.json", "icon-192.png", "icon-512.png"];
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(cacheName).then(c => c.addAll(assets)));
+const cacheName = "gridiron-cache-v1";
+const assets = [
+  "index.html",
+  "style.css",
+  "game.js",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
 });
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
 });
